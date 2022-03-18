c0ae3f2654efbfebeede88d69ee5efaa5bb790cb
armaBurton <armanarma@armaburton.com>
2022-03-17 21:09:48 -0700

    5/2 app now deletes an object from db

--- __tests__/app2.test.js
+++ __tests__/app2.test.js
@@ -95,4 +95,25 @@ describe('alchemy-app routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('should delete an object', async () => {
+    const expected = {
+      id: expect.any(String),
+      manufacturer: 'Holybro',
+      stack_name: 'Kakute H7 Mini Stack',
+      fc_name: 'Kakute H7 FC',
+      esc_name: 'Tekko32 F4 45A ESC',
+      input_voltage: '2-6s',
+      mounting: '20x20mm',
+      cost: '$118.99',
+      backordered: true,
+      img: 'https://cdn.getfpv.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/h/o/holybro-mini-stack---kakute-h7-fc-_-tekko32-f4-45a-esc---20x20-main.jpg'
+    };
+
+    const res = await request(app)
+      .delete('/api/v1/flight_controllers/1');
+
+    expect(res.body).toEqual(expected);
+    expect(await FlightController.getById(1)).toBeNull();
+  });
+
 });
