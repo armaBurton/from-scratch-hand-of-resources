330acabfcdce8f678b2277330e83e7a81c5cf7b6
armaBurton <armanarma@armaburton.com>
2022-03-18 13:03:36 -0700

    2/3 app returns a dragon that matched id

--- __tests__/app3.test.js
+++ __tests__/app3.test.js
@@ -21,4 +21,30 @@ describe('alchemy-app routes', () => {
   
     expect(res.body).toEqual(expected);
   });
+
+  it('should return a single dragon object that matches an ID', async () => {
+    const expected = {
+      id:expect.any(String),
+      age:'Ancient',
+      color:'Black',
+      description:'Gargantuan dragon, chaotic evil',
+      ac:22,
+      hp:'21d20+147',
+      speed:['40 ft', 'fly 80 ft', 'swim 40 ft'],
+      stats:{
+        STR: '27(+8)',
+        DEX: '14(+2)',
+        CON: '25(+7)',
+        INT: '16(+3)',
+        WIS: '15(+2)',
+        CHA: '19(+4)'
+      }
+    };
+
+    const res = await request(app)
+      .get('/api/v1/dragons/2');
+
+    expect(res.body).toEqual(expected);
+  });
+
 });
